f8cbfde48e75c7d5d815c892eb2d95ef86b11ae0
Seo Donggyun <seodonggyun7@gmail.com>
2020-07-13 21:57:16 +0900

    change words

--- remains/donggyun.js
+++ remains/donggyun.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hello!!!');
+  res.send('Hello!!!!!');
 });
 
 module.exports = router;
