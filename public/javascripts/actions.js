d058f9f3dab7e817f7fde51145bf65e361763632
Seo Donggyun <seodonggyun7@gmail.com>
2020-08-03 15:12:18 +0900

    mySQL 적용

--- public/javascripts/actions.js
+++ public/javascripts/actions.js
@@ -1,24 +1,24 @@
 function colorChange (division) {
-    division.style.background = "#F9E29C";
+  division.style.background = '#F9E29C'
 }
 
 function originalColor (division) {
-    division.style.background = "antiquewhite";
+  division.style.background = 'antiquewhite'
 }
 
 function weatherOutput (inputDate) {
-    console.log(inputDate)
-    fetch('http://localhost:3000/weather')
-    .then(res => res.json())
-    .then(data => {
-        document.getElementById(inputDate).innerHTML = data[0].weather;
-    })
+  console.log(inputDate)
+  fetch('http://localhost:3000/weather')
+  .then(res => res.json())
+  .then(data => {
+    document.getElementById(inputDate).innerHTML = data[0].weather
+  })
 }
 
 function dayOutput (inputDate) {
-    fetch('http://localhost:3000/weather')
-    .then(res => res.json())
-    .then(data => {
-        document.getElementById(inputDate).innerHTML = data[0].month + ' ' + data[0].day;
-    })
+  fetch('http://localhost:3000/weather')
+  .then(res => res.json())
+  .then(data => {
+    document.getElementById(inputDate).innerHTML = data[0].month + ' ' + data[0].day;
+  })
 } 
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const pool = require('../utils/mysql').default
+const pool = require('../utils/mysql')
 
 /* GET users listing. */
 router.get('/', async (req, res, next) => {
@@ -20,34 +20,5 @@ router.get('/', async (req, res, next) => {
     res.json({ status: 500, msg: 'error' })
   }
 })
-/*
-router.get('/', (req, res, next) => {
-  connection.query('SELECT * FROM ACCOUNT_TB', (_err, results) => {
-    let sum = 0
-    for (const user of results) {
-      sum += user.money
-    }
-    const avg = sum / results.length
-    connection.query('SELECT * FROM ACCOUNT_TB WHERE money > ?', [avg], (_err, results) => {
-      res.json({ status: 200, arr: results })
-    })
-  })
-})
-*/
-
-/*
-router.get('/kaka', function(req, res, next) {
-  res.json({status: 200, msg: 'success'});
-  // let obj = {'a':1, 'b':2};
-  // obj.a == 1
-});
-
-router.get('/giveme', function(req, res, next) {
-  let number = Math.random();
-  res.json({status: number, msg: 'success'});
-  // let obj = {'a':1, 'b':2};
-  // obj.a == 1
-});
-*/
 
 module.exports = router
